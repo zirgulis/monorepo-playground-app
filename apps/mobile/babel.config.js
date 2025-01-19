9111489da64f248220be5a6c2eb6b1f2a5ca4ce5
Povilas Zirgulis <pzirgulis@gmail.com>
2025-01-19 23:55:32 +0200

    init RN app module

---
+++ apps/mobile/babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@monorepo-playground-app/components': '../../packages/components/src',
+          '@monorepo-playground-app/utils': '../../packages/utils/src',
+        },
+      },
+    ],
+  ],
+};
