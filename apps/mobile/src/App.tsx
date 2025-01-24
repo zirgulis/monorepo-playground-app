a3d00f912035f510a68182eef9220e3ef4559003
Povilas Zirgulis <pzirgulis@gmail.com>
2025-01-24 10:19:50 +0200

    remove gesture handler

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
 import { RootStackParamList } from './navigation/types';
 import { HomeScreen } from './screens/HomeScreen';
@@ -11,20 +10,14 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const App = () => {
   return (
-    <GestureHandlerRootView style={styles.root}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Todo List' }} />
-            <Stack.Screen
-              name="AddTodo"
-              component={AddTodoScreen}
-              options={{ title: 'Add Todo' }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Todo List' }} />
+          <Stack.Screen name="AddTodo" component={AddTodoScreen} options={{ title: 'Add Todo' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
