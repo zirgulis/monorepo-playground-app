af597bd50d59ece7fd75d98b3d06b03d7aa1d3f2
Povilas Zirgulis <pzirgulis@gmail.com>
2025-01-27 22:07:48 +0200

    move mobile folder to packages

---
+++ packages/mobile/src/App.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RootStackParamList } from './navigation/types';
+import { HomeScreen } from './screens/HomeScreen';
+import { AddTodoScreen } from './screens/AddTodoScreen';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export const App = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Todo List' }} />
+          <Stack.Screen name="AddTodo" component={AddTodoScreen} options={{ title: 'Add Todo' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
